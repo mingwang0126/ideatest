a76d8765eb9b7c926b2fc2fd03b7307c1fc64297
imok <1156839887@qq.com>
2022-03-19 20:20:54 +0800

    第一次上传

---
+++ src/com/imok/hello.java
@@ -0,0 +1,12 @@
+package com.imok;
+
+/**
+ * @author imok
+ * @version v.1.0
+ * @date 2022/3/19 20:19
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
